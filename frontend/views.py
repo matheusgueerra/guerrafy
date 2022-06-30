ca293141833f1a66afe09e06ae65af3b54f5ecf5
Matheus Guerra <matheus_rocha87@hotmail.com>
2022-06-30 17:08:15 -0300

    working on frontend

--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(requests, *args, **kwargs):
+def index(request, *args, **kwargs):
     return render(request, 'frontend/index.html')
